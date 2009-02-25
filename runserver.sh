14745016076c3ee250dbfbfa412e9208318c7220
QingFeng <paradise.qingfeng@gmail.com>
2009-02-25 23:59:18 +0800

    Run tokyo tyrant script.

---
+++ runserver.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ttserver -host 61.135.214.29 -port 11211 -thnum 8 -dmn -pid /home/yanxu/khufu/ttserver.pid -log /home/yanxu/khufu/ttserver.log -le -ulog /home/yanxu/khufu/ -ulim 128m -sid 1 -rts /home/yanxu/khufu/ttserver.rts /home/yanxu/khufu/khufu/dystopia.tch#bnum=1000000
+
