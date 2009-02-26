819ece2256a55eef31e4c93909cf646895c02b87
hexuotzo <hexuotzo@gmail.com>
2009-02-26 16:51:03 +0800

    添加分析后，入库

--- htmlfilter.py
+++ htmlfilter.py
@@ -1,11 +1,23 @@
 # encoding: utf-8
 import os
 import glob
+import hashlib
+from html2text import html2text
 for d,subdir,files in os.walk('/home/hexuotzo/khufu/www.zaojiao.com'): 
     #dir:文件夹名 
     #subdir子文件夹 
     #files所有文件
     for f in files:
         fname = os.path.join(d,f)
-        r=open(fname).read()
+        try:
+            r=open(fname).read().decode('utf8')
+        except:
+            try:
+                r=open(fname).read().decode('gbk')
+            except:
+                #print fname
+                continue
+        text = html2text(r)
+        key = hashlib.md5(fname).hexdigest()
+        os.popen("dystmgr put khufu 1%s %s"%(key,text.encode('utf8')))
         
