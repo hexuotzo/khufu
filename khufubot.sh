3d25edc1d8b4e319bd661c59b7b5a7c761b3c422
QingFeng <paradise.qingfeng@gmail.com>
2009-02-26 23:42:29 +0800

    Add khufubot.sh example

--- khufubot.sh
+++ khufubot.sh
@@ -3,5 +3,6 @@
 #e.g
 #./khufubot.sh http://xxx.com/
 #./khufubot.sh -P /tmp/ http://xxx.com/
+#./khufubot.sh -P /tmp -b -o http://xxx.com/
 
 wget -c -r -l 3 -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)" --proxy on $*
