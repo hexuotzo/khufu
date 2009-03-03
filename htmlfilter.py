f27193e23bcdf8dd22a1bfb178bbbeaf88c30c8d
hexuotzo <hexuotzo@gmail.com>
2009-03-03 15:01:48 +0800

    key and text put in khufu, key-url-title-text-html-datetime  put in metaDB

--- htmlfilter.py
+++ htmlfilter.py
@@ -1,6 +1,9 @@
 # encoding: utf-8
 import os,sys
 import hashlib
+import cjson
+import datetime
+import re
 from html2text import html2text
 
 def findpath(path):
@@ -21,12 +24,27 @@ def readtext(f):
             r=u''
     return r
 
+def readtitle(fname):
+    s=open(fname).read()
+    s=s.strip()
+    for title in re.findall(r'<title>(.*)</title>',s):
+        return title    
+
 path = sys.argv[1]
 for d,f in findpath(path):
     fname = os.path.join(d,f)
     r=readtext(fname)
     if r=='':continue
-    text = html2text(r).replace('"','')
-    key = hashlib.md5(fname).hexdigest()
-    print os.popen('dystmgr put khufu 1%s "%s"'%(key,text.encode('utf8'))).read()
-               
+    try:
+        text = html2text(r).replace('"','')
+    except:
+        text = r.replace('"','')
+    key = "1%s" % hashlib.md5(fname).hexdigest()
+    nowtime=datetime.datetime.now()
+    ttl = readtitle(fname)
+    dbvalue=cjson.encode({"title":ttl,"url":fname,"html":r,"text":text,"datetime":str(nowtime)})
+    dbvalue = dbvalue.replace('"','')
+    os.popen('dystmgr put khufu %s "%s"'%(key,text.encode('utf8'))).read()
+    os.popen('tchmgr put metaDB.tch %s "%s"'%(key,dbvalue)).read()
+    print key
+    
