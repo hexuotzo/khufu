b8a3dc9a2ebe97332428b93d01b9dd1d60c6d140
QingFeng <paradise.qingfeng@gmail.com>
2009-03-04 16:08:56 +0800

    增加了put,get的实现

--- py-khufu/py-khufu.py
+++ py-khufu/py-khufu.py
@@ -7,7 +7,8 @@ class PyDystopia(object):
         self.dbname = dbname
         self.lib=CDLL('libtokyodystopia.so')
         self.idb=self.lib.tcidbnew()
-        ecode=self.lib.tcidbopen(self.idb,self.dbname)
+        #IDBOWRITER | IDBOCREAT
+        ecode=self.lib.tcidbopen(self.idb,self.dbname,6)
     def search(self,text,returntext=False):
         """search text"""
         lib = self.lib
@@ -18,12 +19,22 @@ class PyDystopia(object):
         for i in range(rnum.value):
             if returntext:
                 lib.tcidbget.restype = c_char_p
-                text = lib.tcidbget(idb,c_int64(result[i]))
-                yield result[i],text
+                stext = lib.tcidbget(idb,c_int64(result[i]))
+                yield result[i],stext
             yield result[i]
+    def put(self,kid,text):
+        return self.lib.tcidbput(self.idb,c_int64(kid),text)
+    def commit(self):
+        return self.lib.tcidbclose(self.idb)
+    def get(self,kid):
+        self.lib.tcidbget.restype = c_char_p
+        return self.lib.tcidbget(self.idb,c_int64(kid))
 
 if __name__ == '__main__':
     pd = PyDystopia()
     for kid in pd.search('严旭'):
         print "kid",kid
+    print pd.put(5,'用Python插入的')
+    print pd.put(6,'zhmocean脱耦和求偶的心理学差异...风马牛不相及的事情却有着千丝万缕的联系...')
+    print pd.get(1)
 
