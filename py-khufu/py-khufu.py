4e3aac6f72a81d82d4e45f9cb446fda1e4751e5c
QingFeng <paradise.qingfeng@gmail.com>
2009-03-04 14:09:16 +0800

    将程序封装为类的形式,方便追加方法

--- py-khufu/py-khufu.py
+++ py-khufu/py-khufu.py
@@ -1,20 +1,29 @@
 # encoding: utf-8
-import ctypes
+from ctypes import *
 
-def search(text,dbname='khufu'):
-    lib=ctypes.CDLL('libtokyodystopia.so')
-    idb=lib.tcidbnew()
-    ecode=lib.tcidbopen(idb,dbname)
-    rnum=ctypes.c_int()
-    lib.tcidbsearch2.restype=ctypes.POINTER(ctypes.c_uint64)
-    result=lib.tcidbsearch2(idb,text,ctypes.byref(rnum))
-    # lib.tcidbget.restype = ctypes.c_char_p
-    for i in range(rnum.value):
-        # text = lib.tcidbget(idb,ctypes.c_int64(result[i]))
-        # print "text",text
-        yield result[i]
+class PyDystopia(object):
+    """Tokyo Dystopia Python Interface"""
+    def __init__(self, dbname='khufu'):
+        self.dbname = dbname
+        self.lib=CDLL('libtokyodystopia.so')
+        self.idb=self.lib.tcidbnew()
+        ecode=self.lib.tcidbopen(self.idb,self.dbname)
+    def search(self,text,returntext=False):
+        """search text"""
+        lib = self.lib
+        idb = self.idb
+        rnum=c_int()
+        lib.tcidbsearch2.restype=POINTER(c_uint64)
+        result=lib.tcidbsearch2(idb,text,byref(rnum))
+        for i in range(rnum.value):
+            if returntext:
+                lib.tcidbget.restype = c_char_p
+                text = lib.tcidbget(idb,c_int64(result[i]))
+                yield result[i],text
+            yield result[i]
 
 if __name__ == '__main__':
-    for kid in search('严旭'):
+    pd = PyDystopia()
+    for kid in pd.search('严旭'):
         print "kid",kid
 
