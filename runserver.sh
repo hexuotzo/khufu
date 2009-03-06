0c71a97f9acc2433f184bbcac807351ae4f2edd6
QingFeng <paradise.qingfeng@gmail.com>
2009-03-06 13:57:31 +0800

    合并了线上的启动脚本

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ttserver -host 61.135.214.29 -port 11211 -thnum 8 -dmn -pid /home/yanxu/khufu/ttserver.pid -log /home/yanxu/khufu/ttserver.log -le -ulog /home/yanxu/khufu/ -ulim 128m -sid 1 -rts /home/yanxu/khufu/ttserver.rts /home/yanxu/khufu/khufu/dystopia.tch#bnum=1000000
+ttserver -host 61.135.214.29 -port 11211 -thnum 8 -dmn -pid /home/yanxu/khufu/meta/ttserver.pid -log /home/yanxu/khufu/meta/ttserver.log -le -ulog /home/yanxu/khufu/meta -ulim 128m -sid 1 -rts /home/yanxu/khufu/meta/ttserver.rts /home/yanxu/khufu/meta/khufu.tch#bnum=1000000
 
