9ea83d467d48d25fab4fc360514df3bc35b85058
hexuotzo <hexuotzo@gmail.com>
2009-03-12 15:44:04 +0800

    judge body by first word

--- zhengwen.py
+++ zhengwen.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 import os
 from html2text import html2text
-
+bodyss=[]
 url = "http://www.zaojiao.com/education/2008/1120/article_2079.html"
 html = os.popen("curl --compressed %s" % url).read()
 try:
@@ -13,9 +13,12 @@ try:
 except:
     txt = txt.encode('gbk').split('\n')
 for r in txt:
-    print r[0][0]
-    
-        
-        
-        
-        
+    try:
+        r = r.strip().decode('utf8')
+    except:
+        r = r.strip().decode('gbk')
+    if r == '':continue
+    elif type(r[0])==int:bodyss.append(r)
+    #elif type(r[0])==str:yield r
+    elif r[0]== '*' and r[1]=='*': bodyss.append(r)
+print bodyss
