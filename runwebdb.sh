b40ce4ba1b32a327e305c0c95d07316998674334
QingFeng <paradise.qingfeng@gmail.com>
2009-03-13 22:45:28 +0800

    Web db

---
+++ runwebdb.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ttserver -host 114.113.30.29 -port 11212 -thnum 8 -dmn -pid /home/yanxu/khufu/webdb/ttserver.pid -log /home/yanxu/khufu/webdb/ttserver.log -le -ulog /home/yanxu/khufu/webdb -ulim 128m -sid 1 -rts /home/yanxu/khufu/webdb/ttserver.rts /home/yanxu/khufu/webdb/khufu.tch#bnum=1000000
+
