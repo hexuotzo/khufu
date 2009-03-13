ab52ef636d554996aa972ecbd099f94f207378fc
QingFeng <paradise.qingfeng@gmail.com>
2009-03-13 17:22:49 +0800

    完成了建立栏目Cache的脚本,使用方法:
    python webdb.py 0-1岁
    
    即建立完了0-1岁这个栏目.
    
    在程序中只需
    data = cjson.decode(mc.get( hashlib.md5("0-1岁").hextdigest() ))
    
    模板中
    {% for d in data %}
     <a href="/v/{{d.kid}}>"{{d.title}}</a>
    {%endfor %}
    即可

--- webdb.py
+++ webdb.py
@@ -3,22 +3,26 @@
 from pykhufu import PyDystopia
 import cmemcache as memcache
 import cjson
-import sys,os
+import sys,os,hashlib
 
 def removetext(text):
     text = text.encode('utf8')
     return text.replace('-育儿早教-中国早教网','')
 
-word = sys.argv[1]
+def search(word):
+    for kid in os.popen('dystmgr search -nl khufu %s' % word).read().split('\n'):
+        if kid=='':continue
+        obj = cjson.decode(mc.get(kid))
+        d = dict(
+            title = unicode(removetext(obj['title']),'utf8'),
+            kid = kid
+        )
+        yield kid,d
+
+word = sys.argv[1].strip()
 mc = memcache.Client(['114.113.30.29:11211'])
+data = [d for kid,d in search(word)]
+obj = cjson.encode(data)
 mc2 = memcache.Client(['114.113.30.29:11212'])
-for kid in os.popen('dystmgr search -nl khufu %s' % word).read().split('\n'):
-    if kid=='':continue
-    print kid
-    obj = cjson.decode(mc.get(kid))
-    obj2 = cjson.encode(dict(
-        title = removetext(obj['title']),
-        kid = kid
-    ))
-    mc2.set(kid,obj2)
-    
+k = hashlib.md5(word).hexdigest()
+mc2.set(k,obj)
