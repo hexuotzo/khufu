5b658d832c30e60061877e7127d6b6aea9ea5644
QingFeng <paradise.qingfeng@gmail.com>
2009-03-17 00:05:02 +0800

    增加了新的示例

--- khufubot.sh
+++ khufubot.sh
@@ -3,6 +3,7 @@
 #e.g
 #./khufubot.sh http://xxx.com/
 #./khufubot.sh -P /tmp/ http://xxx.com/
-#./khufubot.sh -P /tmp -b -o http://xxx.com/
+#./khufubot.sh -P /tmp -b -o -o web/log http://xxx.com/
+#./khufubot.sh -P /tmp -b -F -E -o web/log http://xxx.com/
 
 wget -c -r -l 3 -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)" --proxy on $*
