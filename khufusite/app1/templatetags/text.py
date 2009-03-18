af8a3f98eab9676f54b2ff261748a3639c42fef8
QingFeng <paradise.qingfeng@gmail.com>
2009-03-18 22:38:57 +0800

    让filter可以

--- khufusite/app1/templatetags/text.py
+++ khufusite/app1/templatetags/text.py
@@ -1,9 +1,10 @@
 #encoding: utf-8
 
 from django import template
-from django.template.defaultfilters import stringfilter
 
-@stringfilter
+register = template.Library()
+
+@register.filter
 def removetext(text):
     text=text.replace("-育儿早教-中国早教网","")
     text=text.replace("-怀孕胎教-中国早教网","")
