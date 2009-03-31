6d2db0baf79066e11e6f00c79efef9b227f0c55d
QingFeng <paradise.qingfeng@gmail.com>
2009-03-31 23:59:44 +0800

    Add Makefile

--- py-khufu/pykhufu.c
+++ py-khufu/pykhufu.c
@@ -5,23 +5,51 @@
 #include <stdint.h>
 
 static PyObject *
-search(PyObject *self, PyObject *args)
-{
+put(PyObject *self,PyObject *args){
+    const char *dbname;
     const char *stext;
+    const int *kid;
+    int ecode;
+    bool result;
+    TCIDB *idb;
+    if (!PyArg_ParseTuple(args, "sis", &dbname, &kid, &stext))
+        return NULL;
+    /* create the object */
+    idb = tcidbnew();
+    /* open the database */
+    if(!tcidbopen(idb, dbname, IDBOCREAT | IDBOWRITER)){
+        ecode = tcidbecode(idb);
+        fprintf(stderr, "open error: %s\n", tcidberrmsg(ecode));
+    }
+    result = tcidbput(idb,(int64_t)kid,stext);
+    /* close the database */
+    if(!tcidbclose(idb)){
+        ecode = tcidbecode(idb);
+        fprintf(stderr, "close error: %s\n", tcidberrmsg(ecode));
+    }
+    /* delete the object */
+    tcidbdel(idb);
+    return Py_BuildValue("b",result);
+}
+
+static PyObject *
+search(PyObject *self, PyObject *args){
+    const char *stext;
+    const char *dbname;
     TCIDB *idb;
     int ecode, rnum, i;
     uint64_t *result;
     char *text;
     PyObject* pList;
     
-    if (!PyArg_ParseTuple(args, "s", &stext))
+    if (!PyArg_ParseTuple(args, "ss", &dbname, &stext))
         return NULL;
 
     /* create the object */
     idb = tcidbnew();
 
     /* open the database */
-    if(!tcidbopen(idb, "/home/yanxu/khufu/khufu", IDBOREADER | IDBONOLCK)){
+    if(!tcidbopen(idb, dbname, IDBOREADER | IDBONOLCK)){
         ecode = tcidbecode(idb);
         fprintf(stderr, "open error: %s\n", tcidberrmsg(ecode));
     }
@@ -30,7 +58,8 @@ search(PyObject *self, PyObject *args)
     pList = PyList_New(rnum);
     if(result){
         for(i = 0; i < rnum; i++){
-            PyList_SetItem(pList, i, Py_BuildValue("i", result[i]));
+            // printf("r[i]:%lld\n",result[i]);
+            PyList_SetItem(pList, i, Py_BuildValue("i", (int)result[i]));
         }
         tcfree(result);
     } else {
@@ -52,6 +81,7 @@ search(PyObject *self, PyObject *args)
 
 PyMethodDef methods[] = {
   {"search", search, METH_VARARGS},
+  {"put", put, METH_VARARGS},
   {NULL, NULL},
 };
 
