64f07f96a5a39c94a1ec1aa4ebb6d9b41d7fd334
QingFeng <paradise.qingfeng@gmail.com>
2009-04-01 15:11:07 +0800

    补充了缺少的全路径

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ttserver -host 114.113.30.29 -port 11211 -thnum 8 -dmn -pid /home/yanxu/khufu/meta/ttserver.pid -log /home/yanxu/khufu/meta/ttserver.log -le -ulog /home/yanxu/khufu/meta -ulim 128m -sid 1 -rts /home/yanxu/khufu/meta/ttserver.rts /home/yanxu/khufu/meta/khufu.tch
+/usr/local/bin/ttserver -host 114.113.30.29 -port 11211 -thnum 8 -dmn -pid /home/yanxu/khufu/meta/ttserver.pid -log /home/yanxu/khufu/meta/ttserver.log -le -ulog /home/yanxu/khufu/meta -ulim 128m -sid 1 -rts /home/yanxu/khufu/meta/ttserver.rts /home/yanxu/khufu/meta/khufu.tch
 
