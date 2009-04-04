bfd8e7038b2d38bce42ad40bd18f9478409e17b7
QingFeng <paradise.qingfeng@gmail.com>
2009-04-04 23:40:51 +0800

    将首页底部二级菜单的部分,改为代码生成,方便修改

--- khufusite/app1/templatetags/text.py
+++ khufusite/app1/templatetags/text.py
@@ -22,3 +22,35 @@ def activemenu2(word,text):
     if word==text:
         return 'class="active2"'
     return ""
+
+@register.simple_tag
+def index2menu():
+    l = [
+        "生活提示",
+        "母胎变化",
+        "适时胎教",
+        "营养保健",
+        "疾病护理",
+        "常见问题"
+    ]
+    menu = [
+        "备孕",
+        "怀孕",
+        "产后",
+        "0-1岁",
+        "1-2岁",
+        "2-3岁",
+        "3-6岁",
+        "专家咨询",
+    ]
+    html_tmpl = '''
+	<ul class="margin_left">
+		<li><h5><a href="/keyword/?insearch=%(m)s">%(m)s</a></h5></li>
+		<li><a href="/keyword/?insearch=%(m)s&amp;type_class=%(n)s">%(n)s</a></li><li><a href="/keyword/?insearch=%(m)s&amp;type_class=%(n)s">%(n)s</a></li><li><a href="/keyword/?insearch=%(m)s&amp;type_class=%(n)s">%(n)s</a></li><li><a href="/keyword/?insearch=%(m)s&amp;type_class=%(n)s">%(n)s</a></li><li><a href="/keyword/?insearch=%(m)s&amp;type_class=%(n)s">%(n)s</a></li><li><a href="/keyword/?insearch=%(m)s&amp;type_class=%(n)s">%(n)s</a></li>
+	</ul>
+    '''
+    html = ''
+    for m in menu:
+        for n in l:
+            html += html_tmpl % dict(m=m,n=n)
+    return html
