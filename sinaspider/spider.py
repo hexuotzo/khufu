4bac8629ca92691c22e99e83fdfbe88283a9180c
QingFeng <paradise.qingfeng@gmail.com>
2009-05-04 15:16:09 +0800

    让爬虫可用

--- sinaspider/spider.py
+++ sinaspider/spider.py
@@ -2,11 +2,24 @@
 # -*- coding: utf8 -*-
 from conf import *
 from html2text import html2text
+from datetime import datetime
 import cPickle as pickle
+import uid
+import os
 
+def indb(title,url,body):
+	url = "http://baby.sina.com.cn%s" % url
+	title = title.encode("utf8")
+	kid = uid.getKid(url)
+	body = html2text(body)
+	tag = c1.classify(body)
+	print kid,title,url,tag
+	os.popen(cmd % (kid,title,now.date(),tag))
 	
 if __name__ == '__main__':
 	c1 = pickle.load(open('../newtags/results.pickle'))
+	cmd = 'tctmgr put ../infodb/infodb %s "title" "%s" "savedate" "%s" "tag1" "%s"'
+	now = datetime.now()
     
 	for title,dic in index_dic_type_one.items():
 		spiderUrl = '%s/%s/%s'%(url, title, dic.get('url'))
@@ -25,13 +38,10 @@ if __name__ == '__main__':
 		analy_now_dateaUrl = spiderUrl
 		analy_now_dateaTitle = now_title[0]
 		
-		print analy_now_dateaTitle.decode('gbk'),analy_now_dateaUrl
+        # print analy_now_dateaTitle.decode('gbk'),analy_now_dateaUrl
 		data_lists = getDatalinks(analy_html)
 		for title,url,body in analy_list(data_lists):
-			url = "http://%s" % url
-			body = html2text(body)
-			dtype = c1.classify(body)
-			print "t1",title,url,dtype
+			indb(title,url,body)
 		
 		for links in bar_link_lists:
 			spiderUrl = url+links[0]
@@ -41,10 +51,6 @@ if __name__ == '__main__':
 			bar_html = ''.join(reg(bar_reSyntax,analy_html))
 			data_lists = getDatalinks(analy_html)
 			for title,url,body in analy_list(data_lists):
-				url = "http://%s" % url
-				body = html2text(body)
-				dtype = c1.classify(body)
-				print "t2",title,url
-		
+				indb(title,url,body)
 		
 		
