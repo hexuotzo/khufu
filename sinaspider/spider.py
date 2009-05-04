3db06a921e53aba59bc463cd78e2351042a9c61e
QingFeng <paradise.qingfeng@gmail.com>
2009-05-04 15:16:09 +0800

    让爬虫可用
    添加了所有的链接符号
    
    修改了重复变量名称问题
    
    将内容入memcache
    
    space -> tab
    
    导入addpinyin模块

--- sinaspider/spider.py
+++ sinaspider/spider.py
@@ -2,11 +2,31 @@
 # -*- coding: utf8 -*-
 from conf import *
 from html2text import html2text
+from datetime import datetime
+from addpinyin import *
 import cPickle as pickle
+import uid
+import os
+import cmemcache
 
+def indb(title,url,body):
+	url = "http://baby.sina.com.cn%s" % url
+	title = title.encode("utf8")
+	kid = uid.getKid(url)
+	text = html2text(body)
+	pinyin = addpinyin(text)
+	tag = c1.classify(body)
+	print kid,title,url,tag
+	os.popen(cmd % (kid,title,now.date(),tag))
+
+	mc = memcache.Client(['114.113.30.29:11211'])
+	dbvalue=cjson.encode({"title":title,"url":url,"html":body,"text":text,"datetime":str(now),"addpinyin":pinyin,"body":text,"kid":kid})
+	mc.set(str(kid),dbvalue)
 	
 if __name__ == '__main__':
 	c1 = pickle.load(open('../newtags/results.pickle'))
+	cmd = 'tctmgr put ../infodb/infodb %s "title" "%s" "savedate" "%s" "tag1" "%s"'
+	now = datetime.now()
     
 	for title,dic in index_dic_type_one.items():
 		spiderUrl = '%s/%s/%s'%(url, title, dic.get('url'))
@@ -25,13 +45,10 @@ if __name__ == '__main__':
 		analy_now_dateaUrl = spiderUrl
 		analy_now_dateaTitle = now_title[0]
 		
-		print analy_now_dateaTitle.decode('gbk'),analy_now_dateaUrl
+        # print analy_now_dateaTitle.decode('gbk'),analy_now_dateaUrl
 		data_lists = getDatalinks(analy_html)
-		for title,url,body in analy_list(data_lists):
-			url = "http://%s" % url
-			body = html2text(body)
-			dtype = c1.classify(body)
-			print "t1",title,url,dtype
+		for title,nurl,body in analy_list(data_lists):
+			indb(title,nurl,body)
 		
 		for links in bar_link_lists:
 			spiderUrl = url+links[0]
@@ -40,11 +57,7 @@ if __name__ == '__main__':
 			bar_reSyntax = dic.get('re')[0]
 			bar_html = ''.join(reg(bar_reSyntax,analy_html))
 			data_lists = getDatalinks(analy_html)
-			for title,url,body in analy_list(data_lists):
-				url = "http://%s" % url
-				body = html2text(body)
-				dtype = c1.classify(body)
-				print "t2",title,url
-		
+			for title,nurl,body in analy_list(data_lists):
+				indb(title,nurl,body)
 		
 		
