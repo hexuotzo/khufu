d12909a9112b236667122199e32f90b4bb37eadc
QingFeng <paradise.qingfeng@gmail.com>
2009-05-06 15:52:17 +0800

    修正了sleep语法错误的问题

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 kill `cat /home/yanxu/khufu/meta/ttserver.pid`
-sleep(2)
+sleep 2
 
 /usr/local/bin/ttserver -host 114.113.30.29 -port 11211 -thnum 8 -dmn -pid /home/yanxu/khufu/meta/ttserver.pid -log /home/yanxu/khufu/meta/ttserver.log -le -ulog /home/yanxu/khufu/meta -ulim 128m -sid 1 -rts /home/yanxu/khufu/meta/ttserver.rts /home/yanxu/khufu/meta/khufu.tch
 
--- runwebdb.sh
+++ runwebdb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 kill `cat /home/yanxu/khufu/webdb/ttserver.pid`
-sleep(2)
+sleep 2
 
 /usr/local/bin/ttserver -host 114.113.30.29 -port 11212 -thnum 8 -dmn -pid /home/yanxu/khufu/webdb/ttserver.pid -log /home/yanxu/khufu/webdb/ttserver.log -le -ulog /home/yanxu/khufu/webdb -ulim 128m -sid 1 -rts /home/yanxu/khufu/webdb/ttserver.rts /home/yanxu/khufu/webdb/khufu.tch#bnum=1000000
 
