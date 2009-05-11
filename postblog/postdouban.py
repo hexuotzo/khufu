94cf0899a23945b8ef4af49fc76ebbf832a832e6
QingFeng <paradise.qingfeng@gmail.com>
2009-05-11 23:49:27 +0800

    提交内容脚本基本完成

--- postblog/postdouban.py
+++ postblog/postdouban.py
@@ -1,4 +1,14 @@
-# import twill
+from twill import *
+import random
+import cmemcache as memcache
+
+def randompages():
+    data = pycabinet.search(dbpath,"tag1","怀孕",20000)
+    for i in range(2):
+        mc = memcache.Client(['boypark.cn:11211'])
+        obj = random.choice(data)
+        d = mc.get(obj['kid'])
+        yield d['title'],d['text']
 
 cmd = '''
 go http://www.douban.com/login
@@ -13,4 +23,8 @@ submit
 
 groups = ['/group/yuer/', '/group/Babymamy/', '/group/38772/', '/group/youngman/', '/group/17444/', '/group/mother/', '/group/babyangel/', '/group/73952/', '/group/baobao/', '/group/hunxuebaobao/', '/group/mamab/', '/group/16020/', '/group/16086/', '/group/34832/', '/group/babe/', '/group/159320/']
 
+for g in groups:
+    for title,text in randompages():
+        print title
+    # print execute_string( cmd % (g,title,text) )
 
